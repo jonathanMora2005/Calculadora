d6b11b0dd943d1e6aead120dcda51bd61d7de8be
jonathanMora2005 <161750433+jonathanMora2005@users.noreply.github.com>
2024-10-01 10:00:15 +0200

    Update MainActivity.kt

--- app/src/main/java/com/dam/jonathan/calculadora/MainActivity.kt
+++ app/src/main/java/com/dam/jonathan/calculadora/MainActivity.kt
@@ -1,34 +1,25 @@
-package com.dam.jonathan.calculadora
+package com.example.calculadora
 
+import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.view.View
 import android.widget.TextView
-import androidx.activity.enableEdgeToEdge
-import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
     private lateinit var myTextView: TextView
+    private var displayText = ""
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        myTextView = findViewById(R.id.TEXT)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+
+        myTextView = findViewById(R.id.TEXT)
     }
-    var Text = ""
 
-    fun numeros(view : View){
-        Log.i("Atiqueta",view.tag.toString())
-        Text += view.tag.toString()
-        myTextView.text = Text
-
+    fun numeros(view: View) {
+        Log.i("Etiqueta", view.tag.toString())
+        displayText += view.tag.toString()
+        myTextView.text = displayText
     }
 }
